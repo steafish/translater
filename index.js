a7a1fdc3521b1107f8f26a72a96f5fe0c50fb8f2
vagrant <vagrant@vagrant.vm>
2022-02-06 14:19:26 +0000

    Updated index

--- index.js
+++ index.js
@@ -2,11 +2,5 @@ import Translate from  './Translate.js'
 import TranslateContext from  './TranslateContext.js'
 import TranslateMessage from "./TranslateMessage";
 
-const steafish = () => {
-    return {
-        Translate: Translate,
-        TranslateContext: TranslateContext,
-        TranslateMessage: TranslateMessage
-    }
-}
-export default steafish()
+
+export default  {Translate, TranslateContext, TranslateMessage};
